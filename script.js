af3b1896df2ae699c0b7f79bc475b66b895b2547
alexandru233847 <alexutualexino33@gmail.com>
2024-10-04 16:57:24 +0200

    Check if the word is not a palindrome

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function palindrome(word) {
+    // Convert the word
     const reverseWord = word.toLowerCase().split('').reverse().join('');
+    return word.toLowerCase() === reverseWord;
 }
+
+// Enter a word
+const word = prompt("Enter a word");
+
+// Let's pass the word to the function
+if (palindrome(word)) {
+    console.log(`${word} is a palindrome`);
+} else {
+    console.log(`${word} is not a palindrome`);
+}
+
